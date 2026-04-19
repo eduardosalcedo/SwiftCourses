2d1091754ea203dab454af62048003a252f0eb1d
eduardosalcedo <esalcedo@mdc.edu>
2026-04-18 20:42:44 -0400

    Update Package.swift
    Remove swiftLanguageModes from Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,17 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+/// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "SwiftCourse",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftCourse",
             targets: ["SwiftCourse"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftCourse"
         ),
-
-    ],
-    swiftLanguageModes: [.v6]
+    ]
 )
